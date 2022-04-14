aeb69d92d7500c83a7f1d64d8d6a6bbc409cc7e8
Akuu29 <owner.akiu0529@gmail.com>
2022-04-14 23:14:39 +0900

    add Add tera

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use actix_web::{HttpServer, App, web, get, Responder};
+use actix_web::{HttpServer, web, App, get, Responder, HttpResponse, Error, error};
+use actix_web::web::Data;
+use tera::{Tera, Context};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}")
 }
 
-#[actix_web::main]
+#[get("/")]
+async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = Context::new();
+    ctx.insert("name", "akeu");
+    let view = tmpl.render("base.html", &ctx).unwrap();
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+}
+
+#[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async {"Hello world!"}))
+            .app_data(Data::new(Tera::new("templates/**/*").unwrap()))
+            .service(index)
             .service(greet)
     })
     .bind(("127.0.0.1", 8080))?
